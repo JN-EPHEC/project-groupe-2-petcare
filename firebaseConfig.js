34f9418b1619aaae2994df1236e1f91a4de9d046
Marie Dresse <you@example.com>
2025-11-04 16:29:48 +0100

    w

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD5m4nhQG2axSfKVwlc16YR6G3ujYYbQeU",
+  authDomain: "petcare-2a317.firebaseapp.com",
+  projectId: "petcare-2a317",
+  storageBucket: "petcare-2a317.firebasestorage.app",
+  messagingSenderId: "828878305795",
+  appId: "1:828878305795:web:d763e6771dec4535fa6c36"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
